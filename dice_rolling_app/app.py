15275726e7fd76e6e51bacc8cb6cb61ad521d8fe
millm124 <150197370+millm124@users.noreply.github.com>
2024-04-02 15:04:49 -0700

    added text to know what the wigits do

--- dice_rolling_app/app.py
+++ dice_rolling_app/app.py
@@ -30,25 +30,22 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Widgets App")
+        self.setWindowTitle("Dice Rolling App")
 
         layout = QVBoxLayout()
-        widgets = [
-            QSlider,
-            QComboBox,
-            QComboBox,
-            QPushButton,
 
-        ]
+        dice_num = QLabel("Number of Dice Rolled")
 
-        
         num_slider = QSlider(Qt.Orientation.Horizontal)
         num_slider.setRange(1, 100)
         num_slider.setSingleStep(1)
 
-        operation_box = QComboBox()
-        operation_box.addItems(["Addition", "Multiplication", "Subtraction (L-R)", "ubtraction (R-L)", "division (L-R)", "division (R-L)"])
+        operation = QLabel("Operation Used")
 
+        operation_box = QComboBox()
+        operation_box.addItems(["Addition", "Multiplication", "Subtraction (L -> R)", "Subtraction (R -> L)", "Division (L -> R)", "Division (R -> L)"])
+
+        sides = QLabel("Sides of Dice")
 
         side_box = QComboBox()
         side_box.addItems(["4", "6", "8", "10", "12", "20", "100"])
@@ -56,10 +53,14 @@ class MainWindow(QMainWindow):
         roll_button = QPushButton(text = "Roll", parent=self)
         roll_button.setFixedSize(200, 60)
 
+        layout.addWidget(dice_num)
         layout.addWidget(num_slider)
+        layout.addWidget(operation)
         layout.addWidget(operation_box)
+        layout.addWidget(sides)
         layout.addWidget(side_box)
-        layout.addWidget(roll_button)
+        layout.addWidget(roll_button, alignment = Qt.AlignmentFlag.AlignHCenter)
+        #layout.setContentsMargins(5, 10, 5, 10)
 
         widget = QWidget()
         widget.setLayout(layout)
